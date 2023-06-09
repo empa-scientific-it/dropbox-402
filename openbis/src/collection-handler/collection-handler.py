ce943b2f35a843a4a5cee724247ecb5d376f989a
Simone Baffelli <simone.baffelli@empa.ch>
2023-06-09 15:47:24 +0200

    Improved checks, improved jython script.

--- openbis/src/collection-handler/collection-handler.py
+++ openbis/src/collection-handler/collection-handler.py
@@ -4,37 +4,42 @@ from java.nio.file import Files
 from java.nio.file.attribute import BasicFileAttributes
 from java.text import SimpleDateFormat
 from java.util import Date
+import zipfile
+
 
 def format_date(date):
     """format the date to a string"""
     return SimpleDateFormat("yyyy-MM-dd HH:mm").format(date)
 
+def is_zip(file):
+    """check if the file is a zip file"""
+    return file.getName().endswith(".zip")
+
+def get_creation_time(file):
+    """Gets the creation time from the file using the Java NIO API"""
+    return  Date(Files.readAttributes(file.toPath(), "creationTime")['creationTime'].toMillis())
+
+
 def process(tr):
     print("Started")
     data_set = tr.createNewDataSet()
     incoming = tr.getIncoming()
-    print(incoming)
+    if is_zip(incoming):
     #Get the creation time of the file
-    creation_time = Date(Files.readAttributes(incoming.toPath(), "creationTime")['creationTime'].toMillis())
-    tr.moveFile(incoming.getAbsolutePath(), data_set)
-    # Get the search service
-    search_service = tr.getSearchService()
- 
-    #List all experiments in a project
-    experiments = search_service.listExperiments("/TEST/TEST")
-    print(experiments[0])
+        tr.moveFile(incoming.getAbsolutePath(), data_set)
+        # Get the search service
+        search_service = tr.getSearchService()
+    
+        #List all experiments in a project
+        experiments = search_service.listExperiments("/TEST/TEST")
+        print(experiments[0])
 
-    #Create a new sample and set its experiment
-    smp = tr.createNewSampleWithGeneratedCode("TEST", "MEASUREMENT")
-    smp.setExperiment(experiments[0])
-    smp.setPropertyValue("MEASUREMENT_DATE", format_date(creation_time))
- 
-    # # Search for all samples with a property value determined by the file name; we don't care about the type
-    # samplePropValue = incoming.getName()
-    # samples = search_service.searchForSamples("ORGANISM", samplePropValue, None)
- 
-
-    data_set.setSample(smp)
+        #Create a new sample and set its experiment
+        smp = tr.createNewSampleWithGeneratedCode("TEST", "MEASUREMENT")
+        smp.setExperiment(experiments[0])
+        smp.setPropertyValue("MEASUREMENT_DATE", format_date(get_creation_time(incoming)))
+        #Set the dataset to the sample just created
+        data_set.setSample(smp)
 
  
 
